1f38efae8be968e0a28b0eec8e0b76fb1c51668e
ebrahimnazmul <ebrahimnazmul20032@gmail.com>
2025-06-18 22:40:21 +0600

    commited

--- app/src/main/java/com/example/otpverificationfirebase/OtpVerifyActivity.java
+++ app/src/main/java/com/example/otpverificationfirebase/OtpVerifyActivity.java
@@ -1,24 +1,93 @@
 package com.example.otpverificationfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.mukeshsolanki.OtpView;
+
 public class OtpVerifyActivity extends AppCompatActivity {
 
+    TextView number;
+    AppCompatButton verifyOtpBtn;
+    OtpView otpView;
+
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_otp_verify);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        number=findViewById(R.id.number);
+        verifyOtpBtn=findViewById(R.id.verifyOtpBtn);
+        otpView=findViewById(R.id.otp_view);
+
+
+        Intent intent=getIntent();
+        String intentNumber=intent.getStringExtra("sendOtpNumber");
+        String verificationId=intent.getStringExtra("verificationId");
+        number.setText(intentNumber);
+
+
+        verifyOtpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (otpView.getText().toString().isEmpty()){
+
+                    otpView.setError("Please Enter OTP");
+                }else {
+
+
+                    String otpNumber=otpView.getText().toString().trim();
+
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otpNumber);
+
+                    // Firebase এ sign-in করা
+                    signInWithCredential(credential);
+
+                }
+
+
+            }
         });
+
+
+
+    }//onCreate method ends here
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // OTP verification successful
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                        // এবার চাইলে Dashboard / Home Activity-তে পাঠাতে পারিস
+                        Intent intent = new Intent(OtpVerifyActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(this, "Verification Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/otpverificationfirebase/SendOtpActivity.java
+++ app/src/main/java/com/example/otpverificationfirebase/SendOtpActivity.java
@@ -3,13 +3,21 @@ package com.example.otpverificationfirebase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SendOtpActivity extends AppCompatActivity {
 
@@ -28,18 +36,66 @@ public class SendOtpActivity extends AppCompatActivity {
 
         sendOtpBtn.setOnClickListener(v -> {
 
-            String sendOtpNumber=edNumber.getText().toString().trim();
+            if (edNumber.getText().toString().trim().isEmpty()){
+
+                edNumber.setError("Please Enter a number");
+
+            }else {
 
 
-            Intent intent=new Intent(SendOtpActivity.this, OtpVerifyActivity.class);
-            intent.putExtra("sendOtpNumber",sendOtpNumber);
-            startActivity(intent);
+                String sendOtpNumber=edNumber.getText().toString().trim();
+
+
+               PhoneAuthProvider.getInstance().verifyPhoneNumber("+88" + sendOtpNumber, 120, TimeUnit.SECONDS, this
+                       , new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                           @Override
+                           public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                           }
+
+                           @Override
+                           public void onVerificationFailed(@NonNull FirebaseException e) {
+                               Toast.makeText(SendOtpActivity.this, "Error Is"+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                           }
+
+                           @Override
+                           public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+
+                               Intent intent=new Intent(SendOtpActivity.this, OtpVerifyActivity.class);
+                               intent.putExtra("sendOtpNumber",sendOtpNumber);
+                               intent.putExtra("verificationId",verificationId);
+                               startActivity(intent);
+
+                           }
+                       }
+
+
+               );
+
+
+
+
+
+
+                //animation for open a  new activity
+                overridePendingTransition(R.anim.activity_open_animation, R.anim.activity_open_animation);
+            }
+
+
+
+
+
+
+
 
         });
 
 
-
     }
+
+
+
 
     private  void variableFinder(){
         sendOtpBtn=findViewById(R.id.sendOtpBtn);
